4ff5734ec69d365fd6d763ef1de7e568eca84b93
Leigh Simpson <leigh.simpson@fusepump.com>
2013-02-04 19:48:08 +0000

    Whitespace formatting and code layout changes for Colours class
     - Modified the colouring arrays so they line up neatly
     - Split up long lines within the string() method

--- lib/FusePump/Cli/Colours.php
+++ lib/FusePump/Cli/Colours.php
@@ -8,51 +8,60 @@ namespace FusePump\Cli;
 class Colours
 {
     private static $foreground_colors = array(
-        'black' => '0;30',
-        'dark_gray' => '1;30',
-        'red' => '0;31',
-        'bold_red' => '1;31',
-        'green' => '0;32',
-        'bold_green' => '1;32',
-        'brown' => '0;33',
-        'yellow' => '1;33',
-        'blue' => '0;34',
-        'bold_blue' => '1;34',
-        'purple' => '0;35',
+        'black'       => '0;30',
+        'dark_gray'   => '1;30',
+        'red'         => '0;31',
+        'bold_red'    => '1;31',
+        'green'       => '0;32',
+        'bold_green'  => '1;32',
+        'brown'       => '0;33',
+        'yellow'      => '1;33',
+        'blue'        => '0;34',
+        'bold_blue'   => '1;34',
+        'purple'      => '0;35',
         'bold_purple' => '1;35',
-        'cyan' => '0;36',
-        'bold_cyan' => '1;36',
-        'white' => '1;37',
-        'bold_gray' => '0;37'
+        'cyan'        => '0;36',
+        'bold_cyan'   => '1;36',
+        'white'       => '1;37',
+        'bold_gray'   => '0;37',
     );
 
     private static $background_colors = array(
-        'black' => '40',
-        'red' => '41',
-        'magenta' => '45',
-        'yellow' => '43',
-        'green' => '42',
-        'blue' => '44',
-        'cyan' => '46',
-        'light_gray' => '47'
+        'black'      => '40',
+        'red'        => '41',
+        'magenta'    => '45',
+        'yellow'     => '43',
+        'green'      => '42',
+        'blue'       => '44',
+        'cyan'       => '46',
+        'light_gray' => '47',
     );
 
     // Returns colored string
-    public static function string($string, $foreground_color = null, $background_color = null)
-    {
+    public static function string(
+        $string,
+        $foreground_color = null,
+        $background_color = null
+    ) {
         $colored_string = "";
 
         // Check if given foreground color found
         if (isset(self::$foreground_colors[$foreground_color])) {
-            $colored_string .= "\033[" . self::$foreground_colors[$foreground_color] . "m";
+            $colored_string = $colored_string
+                            . '\033['
+                            . self::$foreground_colors[$foreground_color]
+                            . 'm';
         }
         // Check if given background color found
         if (isset(self::$background_colors[$background_color])) {
-            $colored_string .= "\033[" . self::$background_colors[$background_color] . "m";
+            $colored_string = $colored_string
+                            . '\033['
+                            . self::$background_colors[$background_color]
+                            . 'm';
         }
 
         // Add string and end coloring
-        $colored_string .=  $string . "\033[0m";
+        $colored_string = $colored_string . $string . '\033[0m';
 
         return $colored_string;
     }
